f159f26df13884b3b0d52b56f83e48c4d2b3de00
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-03 13:28:48 -0500

    Centered the main page by setting the inline margin to auto.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Header from "./header";
 
 export default function Page() {
   return (
-    <div className="max-w-[1440px]">
+    <div className="max-w-[1440px] mx-auto">
       <Header />
       <h1 className="font-bold underline text-3xl">
         Welcome to Toast Financial!
