d804acf5879c8b981050f63ace689650dfa22bc1
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-03 11:15:05 -0500

    Added a header component with a nav element.

---
+++ app/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <>
+      <nav>Hello this is a header</nav>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Header from "./header";
+
 export default function Page() {
   return (
     <>
+      <Header />
       <h1>Welcome to Toast Financial!</h1>
     </>
   );
