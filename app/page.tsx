4aad09fba6b897b55e9e73122661101de46db028
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-03 13:25:33 -0500

    Set the maximum width to 1440 px.

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import Header from "./header";
 
 export default function Page() {
   return (
-    <>
+    <div className="max-w-[1440px]">
       <Header />
       <h1 className="font-bold underline text-3xl">
         Welcome to Toast Financial!
       </h1>
-    </>
+    </div>
   );
 }
