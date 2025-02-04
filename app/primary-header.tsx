325c81b2cf5ad7be443ef0a3317dcdb800136c61
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-04 07:02:37 -0500

    Added a column gap to the ul flexbox in PrimaryHeader.

--- app/primary-header.tsx
+++ app/primary-header.tsx
@@ -2,7 +2,7 @@ export default function PrimaryHeader() {
   return (
     <div className="h-[64px]">
       This is a primary header
-      <ul className="flex">
+      <ul className="flex gap-x-[72px]">
         <li>Personal</li>
         <li>Business</li>
       </ul>
