cbf598d5eb9e9e5a1d418ce97e4e5da21fd675f6
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-04 07:25:43 -0500

    Made PrimaryHeader's outermost div a flexbox.

--- app/primary-header.tsx
+++ app/primary-header.tsx
@@ -1,6 +1,6 @@
 export default function PrimaryHeader() {
   return (
-    <div className="h-[64px] px-[80px] w-full">
+    <div className="h-[64px] px-[80px] w-full flex">
       <ul className="flex gap-x-[72px]">
         <li>Personal</li>
         <li>Business</li>
