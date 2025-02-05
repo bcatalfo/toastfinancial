c8e07a53562b40a8bd9e41cf81c8ce30f7e48f37
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-04 23:55:50 -0500

    Changed personal and business to all caps.

--- app/primary-header.tsx
+++ app/primary-header.tsx
@@ -2,8 +2,8 @@ export default function PrimaryHeader() {
   return (
     <div className="h-[64px] px-[80px] w-full flex bg-[#13294b] text-white">
       <ul className="flex gap-x-[72px] items-center">
-        <li>Personal</li>
-        <li>Business</li>
+        <li>PERSONAL</li>
+        <li>BUSINESS</li>
       </ul>
     </div>
   );
