7115c658bd680baad770585bb13d141c67fe2ff0
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-06 09:26:28 -0500

    Moved the 1px letter spacing to the a tag from the li tag.

--- app/primary-header.tsx
+++ app/primary-header.tsx
@@ -2,8 +2,8 @@ export default function PrimaryHeader() {
   return (
     <div className="h-[64px] px-[80px] w-full flex bg-[#13294b] text-white">
       <ul className="font-semibold flex gap-x-[72px] items-center">
-        <li className="tracking-[1px] h-full flex flex-row items-center relative">
-          <a className="uppercase after:bg-[linear-gradient(180deg,#59bec9,#007dba)] after:w-[2px] after:h-[44px] after:transform-[rotate(-90deg)] after:absolute after:top-[24px] after:left-[50%] after:rounded-[8px]">
+        <li className="h-full flex flex-row items-center relative">
+          <a className="tracking-[1px] uppercase after:bg-[linear-gradient(180deg,#59bec9,#007dba)] after:w-[2px] after:h-[44px] after:transform-[rotate(-90deg)] after:absolute after:top-[24px] after:left-[50%] after:rounded-[8px]">
             personal
           </a>
         </li>
