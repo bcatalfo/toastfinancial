7e1cdfd1eb5a46a1d807b35e2d07abc9bbfb840c
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-06 09:22:22 -0500

    Moved font-semibold from the li to the ul.

--- app/primary-header.tsx
+++ app/primary-header.tsx
@@ -1,8 +1,8 @@
 export default function PrimaryHeader() {
   return (
     <div className="h-[64px] px-[80px] w-full flex bg-[#13294b] text-white">
-      <ul className="flex gap-x-[72px] items-center">
-        <li className="font-semibold tracking-[1px] h-full flex flex-row items-center relative">
+      <ul className="font-semibold flex gap-x-[72px] items-center">
+        <li className="tracking-[1px] h-full flex flex-row items-center relative">
           <a className="uppercase after:bg-[linear-gradient(180deg,#59bec9,#007dba)] after:w-[2px] after:h-[44px] after:transform-[rotate(-90deg)] after:absolute after:top-[24px] after:left-[50%] after:rounded-[8px]">
             personal
           </a>
