d1fde7a9a332975b9ab563e6f244e70186263996
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-06 10:56:25 -0500

    Added placeholders for the bread logo and help and resources / about us section.

--- app/primary-header.tsx
+++ app/primary-header.tsx
@@ -1,18 +1,33 @@
 export default function PrimaryHeader() {
   return (
-    <div className="h-[64px] px-[80px] w-full flex bg-[#13294b] text-white">
-      <ul className="font-semibold flex gap-x-[72px] items-center">
-        <li className="bg-[url(/header-rain-image.svg)] h-full flex flex-row items-center relative">
-          <a className="leading-[20px] tracking-[1px] uppercase after:bg-[linear-gradient(180deg,#59bec9,#007dba)] after:w-[2px] after:h-[44px] after:transform-[rotate(-90deg)] after:absolute after:top-[24px] after:left-[50%] after:rounded-[8px]">
-            personal
-          </a>
-        </li>
-        <li>
-          <a className="leading-[17px] text-[12px] tracking-[1px] uppercase">
-            business
-          </a>
-        </li>
-      </ul>
+    <div className="h-[64px] px-[80px] w-full flex bg-[#13294b] text-white items-center justify-between">
+      <div className="h-full flex">
+        <ul className="font-semibold flex gap-x-[72px] items-center">
+          <li className="bg-[url(/header-rain-image.svg)] h-full flex flex-row items-center relative">
+            <a className="leading-[20px] tracking-[1px] uppercase after:bg-[linear-gradient(180deg,#59bec9,#007dba)] after:w-[2px] after:h-[44px] after:transform-[rotate(-90deg)] after:absolute after:top-[24px] after:left-[50%] after:rounded-[8px]">
+              personal
+            </a>
+          </li>
+          <li>
+            <a className="leading-[17px] text-[12px] tracking-[1px] uppercase">
+              business
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <a>Picture goes here</a>
+      </div>
+      <div>
+        <ul className="flex items-center gap-[36px]">
+          <li>
+            <a>Help & Resources</a>
+          </li>
+          <li>
+            <a>About Us</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
