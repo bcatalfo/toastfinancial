75ccee72c91aacb9b05844a151318141f08c97e5
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-05 23:35:10 -0500

    Used a CSS text transform to make the text uppercase.

--- app/primary-header.tsx
+++ app/primary-header.tsx
@@ -1,9 +1,9 @@
 export default function PrimaryHeader() {
   return (
     <div className="h-[64px] px-[80px] w-full flex bg-[#13294b] text-white">
-      <ul className="flex gap-x-[72px] items-center font-semibold">
-        <li>PERSONAL</li>
-        <li>BUSINESS</li>
+      <ul className="flex gap-x-[72px] items-center font-semibold uppercase">
+        <li>personal</li>
+        <li>business</li>
       </ul>
     </div>
   );
