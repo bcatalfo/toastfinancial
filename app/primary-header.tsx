415a72819fc92eba54f6148af272131c297b92d7
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-05 23:48:30 -0500

    Added letter spacing of 1px to PERSONAL.

--- app/primary-header.tsx
+++ app/primary-header.tsx
@@ -2,7 +2,7 @@ export default function PrimaryHeader() {
   return (
     <div className="h-[64px] px-[80px] w-full flex bg-[#13294b] text-white">
       <ul className="flex gap-x-[72px] items-center">
-        <li className="font-semibold uppercase">personal</li>
+        <li className="font-semibold uppercase tracking-[1px]">personal</li>
         <li>business</li>
       </ul>
     </div>
