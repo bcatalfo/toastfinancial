c694a4d87d2f9720a5855a9074468ee724237492
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-06 08:44:50 -0500

    Added a bunch of CSS to primary header in the li tag and some to a new a tag. The CSS in the li fixes positioning with flex, centering items, and relative positioning making the absolute child positioned correctly. The class for the a tag just does the underline with the CSS after pseudoclass.

--- app/primary-header.tsx
+++ app/primary-header.tsx
@@ -2,7 +2,11 @@ export default function PrimaryHeader() {
   return (
     <div className="h-[64px] px-[80px] w-full flex bg-[#13294b] text-white">
       <ul className="flex gap-x-[72px] items-center">
-        <li className="font-semibold uppercase tracking-[1px]">personal</li>
+        <li className="font-semibold uppercase tracking-[1px] h-full flex flex-row items-center relative">
+          <a className="after:bg-[linear-gradient(180deg,#59bec9,#007dba)] after:w-[2px] after:h-[44px] after:transform-[rotate(-90deg)] after:absolute after:top-[24px] after:left-[50%] after:rounded-[8px]">
+            personal
+          </a>
+        </li>
         <li>business</li>
       </ul>
     </div>
