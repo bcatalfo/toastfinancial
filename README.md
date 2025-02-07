b8c5ff214bf4ba030ae7f16ed6aef2f115fe8c14
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-07 09:58:47 -0500

    Added a blue background to the DropdownItem when hovered.

--- README.md
+++ README.md
@@ -13,6 +13,4 @@ Check it out at [toastfinancial.com](https://toastfinancial.com/).
 - - - - Add the arrow svg
 - - - - - Animate the arrow when you hover over the dropdown
 - - - - Make the dropdown look nicer with CSS
-- - - - Add hover effects (CSS or JS?)
-- - - - - Make the background color change to blue when you hover over an element of the dropdown ie Health Center or Financial Education
 - - - Switch to business version when you click on business
--- app/dropdown-item.tsx
+++ app/dropdown-item.tsx
@@ -1,3 +1,6 @@
+import classNames from "classnames";
+import { useState } from "react";
+
 export default function DropdownItem({
   index,
   item,
@@ -5,9 +8,20 @@ export default function DropdownItem({
   index: number;
   item: string;
 }) {
+  const [isActive, setIsActive] = useState(false);
   return (
-    <li key={index}>
-      <a className="text-[#13294b]">{item}</a>
+    <li
+      key={index}
+      onMouseEnter={(_) => setIsActive(true)}
+      onMouseLeave={(_) => setIsActive(false)}
+    >
+      <a
+        className={classNames("text-[#13294b]", {
+          "bg-[rgb(177,228,227)]": isActive,
+        })}
+      >
+        {item}
+      </a>
     </li>
   );
 }
