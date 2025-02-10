361144b74b839792020c0d4325d9df804934bab0
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-10 09:22:42 -0500

    Made the dropdowns and the search signin placeholder horizontal instead of vertical by making the outermost div flex and flex-direction row.

--- app/secondary-header.tsx
+++ app/secondary-header.tsx
@@ -1,6 +1,6 @@
 export default function SecondaryHeader() {
   return (
-    <div className="px-[80px]">
+    <div className="px-[80px] flex flex-row">
       <div>Put dropdowns here</div>
       <div>Put search bar and sign in here</div>
     </div>
