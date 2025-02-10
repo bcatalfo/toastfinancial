e36d25c0cf55d2bf6ddf913c156d7a90997242cc
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-10 12:58:17 -0500

    Added the dropdowns to Credit Cards, Loans, and Savings in SecondaryHeader.

--- app/secondary-header.tsx
+++ app/secondary-header.tsx
@@ -1,11 +1,33 @@
+import Dropdown from "./dropdown";
+
 export default function SecondaryHeader() {
   return (
     <div className="h-[66px] px-[80px] flex flex-row justify-between">
       <div>
         <ul className="flex gap-[36px]">
-          <li>Credit Cards</li>
-          <li>Loans</li>
-          <li>Savings</li>
+          <Dropdown
+            name="Credit Cards"
+            items={[
+              "Explore Credit Cards",
+              "Activate Credit Card",
+              "Toast Cashback Card",
+              "Toast Rewards Card",
+            ]}
+          />
+          <Dropdown
+            name="Loans"
+            items={["Debt Consolidation", "Buy Now, Pay Later"]}
+          />
+          <Dropdown
+            name="Savings"
+            items={[
+              "What We Offer",
+              "High-Yield Savings Account",
+              "Certificates of Deposit",
+              "IRAs: Retirement Savings & CDs",
+              "Forms & Documents",
+            ]}
+          />
         </ul>
       </div>
       <div>Put search bar and sign in here</div>
