8f1494e5db902ae1974327dd650fbc10b59c8d2b
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-10 09:37:39 -0500

    Fixed spacing between Credit Cards, Loans, Savings li tags by setting ul to be flex with 36px gap.

--- app/secondary-header.tsx
+++ app/secondary-header.tsx
@@ -2,7 +2,7 @@ export default function SecondaryHeader() {
   return (
     <div className="px-[80px] flex flex-row justify-between">
       <div>
-        <ul>
+        <ul className="flex gap-[36px]">
           <li>Credit Cards</li>
           <li>Loans</li>
           <li>Savings</li>
