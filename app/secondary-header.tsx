34fb45a5747390f9c9b54266bd0e0c0b603e0555
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-10 09:25:52 -0500

    Added space between dropdown and searchbar/signign placeholder by setting justifty-content to space-between in the outer div.

--- app/secondary-header.tsx
+++ app/secondary-header.tsx
@@ -1,6 +1,6 @@
 export default function SecondaryHeader() {
   return (
-    <div className="px-[80px] flex flex-row">
+    <div className="px-[80px] flex flex-row justify-between">
       <div>Put dropdowns here</div>
       <div>Put search bar and sign in here</div>
     </div>
