c7ae9c2e7dabdd06cdfa1d18e338297010b6e5d2
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-10 09:30:41 -0500

    Added Credit Card, Loans, Sabings to secondary header with a ul and li tags.

--- app/secondary-header.tsx
+++ app/secondary-header.tsx
@@ -1,7 +1,13 @@
 export default function SecondaryHeader() {
   return (
     <div className="px-[80px] flex flex-row justify-between">
-      <div>Put dropdowns here</div>
+      <div>
+        <ul>
+          <li>Credit Cards</li>
+          <li>Loans</li>
+          <li>Savings</li>
+        </ul>
+      </div>
       <div>Put search bar and sign in here</div>
     </div>
   );
