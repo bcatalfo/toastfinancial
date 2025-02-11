2578f3fa7d1bea1b28e4e47f8cd47122082c361b
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-11 15:01:01 -0500

    Changed to script to make it (hopefully) automatically deploy

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     <div className="max-w-[1440px] mx-auto">
       <Header />
       <h1 className="font-bold underline text-3xl">
-        Welcome to Toast Financial!
+        Welcome to Toast Financial! Automatic deployment is working!
       </h1>
     </div>
   );
