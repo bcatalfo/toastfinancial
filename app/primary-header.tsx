bd92b38f752afaa30e6b1721e31a43890a666815
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-11 19:45:43 -0500

    Hide personal / business links on mobile.

--- app/primary-header.tsx
+++ app/primary-header.tsx
@@ -5,7 +5,7 @@ import Dropdown from "./dropdown";
 export default function PrimaryHeader() {
   return (
     <div className="h-[64px] px-[80px] w-full flex bg-[#13294b] text-white items-center justify-between">
-      <div className="h-full flex">
+      <div className="hidden lg:h-full lg:flex">
         <ul className="font-semibold flex gap-x-[72px] items-center">
           <li className="bg-[url(/header-rain-image.svg)] h-full flex flex-row items-center relative">
             <a className="cursor-pointer leading-[20px] tracking-[1px] uppercase after:bg-[linear-gradient(180deg,#59bec9,#007dba)] after:w-[2px] after:h-[44px] after:transform-[rotate(-90deg)] after:absolute after:top-[24px] after:left-[50%] after:rounded-[8px]">
