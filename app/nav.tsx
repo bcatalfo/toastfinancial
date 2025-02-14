2aa5f0d9526b7e88207c236b4e6a90ce20ca06a9
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-13 22:10:37 -0500

    Extracted the JSX for the personal / business selection into a component called Nav.

---
+++ app/nav.tsx
@@ -0,0 +1,16 @@
+export default function Nav() {
+  return (
+    <ul className="font-semibold flex gap-x-[72px] items-center">
+      <li className="bg-[url(/header-rain-image.svg)] h-full flex flex-row items-center relative">
+        <a className="cursor-pointer leading-[20px] tracking-[1px] uppercase after:bg-[linear-gradient(180deg,#59bec9,#007dba)] after:w-[2px] after:h-[44px] after:transform-[rotate(-90deg)] after:absolute after:top-[24px] after:left-[50%] after:rounded-[8px]">
+          personal
+        </a>
+      </li>
+      <li>
+        <a className="cursor-pointer leading-[17px] text-[12px] tracking-[1px] uppercase">
+          business
+        </a>
+      </li>
+    </ul>
+  );
+}
--- app/primary-header.tsx
+++ app/primary-header.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import logo from "../public/Bread Logo.svg";
 import Dropdown from "./dropdown";
+import Nav from "./nav";
 
 export default function PrimaryHeader({
   setSecondaryOpen,
@@ -10,18 +11,7 @@ export default function PrimaryHeader({
   return (
     <div className="h-[64px] px-[24px] lg:px-[40px] xl:px-[80px] w-full flex bg-[#13294b] text-white items-center justify-between">
       <div className="hidden lg:h-full lg:flex">
-        <ul className="font-semibold flex gap-x-[72px] items-center">
-          <li className="bg-[url(/header-rain-image.svg)] h-full flex flex-row items-center relative">
-            <a className="cursor-pointer leading-[20px] tracking-[1px] uppercase after:bg-[linear-gradient(180deg,#59bec9,#007dba)] after:w-[2px] after:h-[44px] after:transform-[rotate(-90deg)] after:absolute after:top-[24px] after:left-[50%] after:rounded-[8px]">
-              personal
-            </a>
-          </li>
-          <li>
-            <a className="cursor-pointer leading-[17px] text-[12px] tracking-[1px] uppercase">
-              business
-            </a>
-          </li>
-        </ul>
+        <Nav />
       </div>
       <div>
         <a className="cursor-pointer">
