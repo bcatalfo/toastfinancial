0ba5052519447743f117c07ae2ab1d6fd389e08a
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-17 02:34:13 -0500

    Gave the open SignIn button a brighter background color.

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -7,7 +7,11 @@ export default function SignIn({ isSecondary }: { isSecondary: boolean }) {
     <>
       <button
         type="button"
-        className="bg-[rgb(28,129,149)] bg-[url(/down-arrow.svg)] bg-[bottom_14.5px_right_12px] bg-no-repeat lg:bg-[bottom_16.5px_right_24px] w-[88.54px] lg:w-[115px] py-[8px] px-[12px] lg:py-[10px] lg:pr-[36.54px] lg:pl-[24px] lg:pb-[10px] text-white font-bold rounded-[40px] text-left cursor-pointer"
+        className={classNames(
+          { "bg-[rgb(28,129,149)]": !isOpen },
+          { "bg-[rgb(89,190,201)]": isOpen },
+          "bg-[url(/down-arrow.svg)] bg-[bottom_14.5px_right_12px] bg-no-repeat lg:bg-[bottom_16.5px_right_24px] w-[88.54px] lg:w-[115px] py-[8px] px-[12px] lg:py-[10px] lg:pr-[36.54px] lg:pl-[24px] lg:pb-[10px] text-white font-bold rounded-[40px] text-left cursor-pointer"
+        )}
         onClick={() => setIsOpen(!isOpen)}
       >
         Sign In
