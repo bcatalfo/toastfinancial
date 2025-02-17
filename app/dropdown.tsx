a67d884a078c2b5adfa32d7ccdc539f62231ec7e
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-17 13:02:51 -0500

    Gave the ul in the dropdown a z-index of 1 so it won't hide under the Sign In button.

--- app/dropdown.tsx
+++ app/dropdown.tsx
@@ -54,7 +54,8 @@ export default function Dropdown({
           { hidden: !isDropdownOpen },
           { "left-[-16px]": isSecondary },
           "bg-[#f4f5f5] flex flex-col w-full",
-          "lg:absolute lg:right-0 lg:top-[27.5px] lg:w-[197.7px] lg:bg-white lg:rounded-[8px] lg:p-[7px] lg:shadow-[0px_8px_20px_rgba(0,0,0,0.15)]"
+          "lg:absolute lg:right-0 lg:top-[27.5px] lg:w-[197.7px] lg:bg-white lg:rounded-[8px] lg:p-[7px] lg:shadow-[0px_8px_20px_rgba(0,0,0,0.15)]",
+          "z-1"
         )}
       >
         {items.map((item, index) => (
