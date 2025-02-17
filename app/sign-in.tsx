e256135ac4913e80131b3542d56754217979768b
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-16 20:10:19 -0500

    Added cursor: pointer to the sign in button.

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -2,7 +2,7 @@ export default function SignIn() {
   return (
     <button
       type="button"
-      className="bg-[rgb(28,129,149)] bg-[url(/down-arrow.svg)] bg-[bottom_14.5px_right_12px] bg-no-repeat lg:bg-[bottom_16.5px_right_24px] w-[88.54px] lg:w-[115px] py-[8px] px-[12px] lg:py-[10px] lg:pr-[36.54px] lg:pl-[24px] lg:pb-[10px] text-white font-bold rounded-[40px] text-left"
+      className="bg-[rgb(28,129,149)] bg-[url(/down-arrow.svg)] bg-[bottom_14.5px_right_12px] bg-no-repeat lg:bg-[bottom_16.5px_right_24px] w-[88.54px] lg:w-[115px] py-[8px] px-[12px] lg:py-[10px] lg:pr-[36.54px] lg:pl-[24px] lg:pb-[10px] text-white font-bold rounded-[40px] text-left cursor-pointer"
     >
       Sign In
     </button>
