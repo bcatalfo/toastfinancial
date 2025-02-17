de37612fd1a6d9f93fdca4eaedd3ae312a5b5fb6
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-17 02:18:14 -0500

    Gave the SignInDropdown a white background on desktop.

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -32,7 +32,7 @@ export function SignInDropdown({ isHidden }: { isHidden: boolean }) {
     <ul
       className={classNames(
         { hidden: isHidden },
-        "flex flex-col lg:absolute bg-[rgb(244,245,245)] rounded-lg"
+        "flex flex-col lg:absolute bg-[rgb(244,245,245)] lg:bg-white rounded-lg"
       )}
     >
       {["Credit Cards", "Savings", "Loans", "Business Sign In"].map((name) => (
