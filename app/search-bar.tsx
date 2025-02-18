65ce0b03aaa169d8895dccc0f45bb078c4f34da2
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-18 15:46:29 -0500

    Set the font size for the input in SearchBar.

--- app/search-bar.tsx
+++ app/search-bar.tsx
@@ -3,7 +3,7 @@ export default function SearchBar() {
     <div className="mt-[12px] mx-[24px] mb-[46px] lg:m-0">
       <input
         placeholder="Search"
-        className="bg-[hsla(0,0%,100%,0.25)] lg:bg-white w-full border-none lg:border-1 lg:border-solid lg:border-[#d0d3d4] placeholder:text-white lg:placeholder:text-[rgb(13,36,66)] rounded-[56px] lg:rounded-3xl py-[16px] pr-[16px] pl-[44px] lg:py-[8px] lg:pr-[16px] lg:pl-[44px]"
+        className="bg-[hsla(0,0%,100%,0.25)] lg:bg-white w-full border-none lg:border-1 lg:border-solid lg:border-[#d0d3d4] placeholder:text-white lg:placeholder:text-[rgb(13,36,66)] rounded-[56px] lg:rounded-3xl py-[16px] pr-[16px] pl-[44px] lg:py-[8px] lg:pr-[16px] lg:pl-[44px] text-[18px] lg:text-[14px]"
       ></input>
     </div>
   );
