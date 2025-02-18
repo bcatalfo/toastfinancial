57096478e0a837ea66dd1557aab6a2b546f8c21a
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-18 14:05:09 -0500

    Gave the div in SearchBar some margin on mobile.

--- app/search-bar.tsx
+++ app/search-bar.tsx
@@ -1,6 +1,6 @@
 export default function SearchBar() {
   return (
-    <div className="">
+    <div className="mt-[12px] mx-[24px] mb-[46px] lg:m-0">
       <input className="bg-[hsla(0,0%,100%,0.25)] lg:bg-white"></input>
     </div>
   );
