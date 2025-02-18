c6d0064dae4bc15f7d1933b3dd7e84a5aea52d62
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-18 14:07:02 -0500

    Gave the input in SearchBar full width on mobile and desktop.

--- app/search-bar.tsx
+++ app/search-bar.tsx
@@ -1,7 +1,7 @@
 export default function SearchBar() {
   return (
     <div className="mt-[12px] mx-[24px] mb-[46px] lg:m-0">
-      <input className="bg-[hsla(0,0%,100%,0.25)] lg:bg-white"></input>
+      <input className="bg-[hsla(0,0%,100%,0.25)] lg:bg-white w-full"></input>
     </div>
   );
 }
