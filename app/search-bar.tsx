e3ca9e766a28f80ba38f86902f30e587fc680fea
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-18 16:01:39 -0500

    Made the input font-color white on mobile and black on desktop.

--- app/search-bar.tsx
+++ app/search-bar.tsx
@@ -3,7 +3,7 @@ export default function SearchBar() {
     <div className="mt-[12px] mx-[24px] mb-[46px] lg:m-0 relative lg:w-[343px]">
       <input
         placeholder="Search"
-        className="bg-[hsla(0,0%,100%,0.25)] lg:bg-white w-full border-none lg:border-1 lg:border-solid lg:border-[#d0d3d4] placeholder:text-white lg:placeholder:text-[rgb(13,36,66)] rounded-[56px] lg:rounded-3xl py-[16px] pr-[16px] pl-[44px] lg:py-[8px] lg:pr-[16px] lg:pl-[44px] text-[18px] lg:text-[14px]"
+        className="bg-[hsla(0,0%,100%,0.25)] lg:bg-white w-full border-none lg:border-1 lg:border-solid lg:border-[#d0d3d4] placeholder:text-white lg:placeholder:text-[rgb(13,36,66)] text-white lg:text-black rounded-[56px] lg:rounded-3xl py-[16px] pr-[16px] pl-[44px] lg:py-[8px] lg:pr-[16px] lg:pl-[44px] text-[18px] lg:text-[14px]"
       ></input>
       <i className="absolute top-[16px] left-[16px] w-[24px] h-[24px] bg-[url(/search-mobile.svg)] lg:top-[10px] lg:left-[16px] lg:w-[20px] lg:h-[20px] lg:bg-[url(/search.svg)]"></i>
     </div>
