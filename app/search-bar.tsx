32bf875d672d433fc470c9af01165795334f0931
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-18 14:01:43 -0500

    Wrapped the SearchBar with a div.

--- app/search-bar.tsx
+++ app/search-bar.tsx
@@ -1,3 +1,7 @@
 export default function SearchBar() {
-  return <input className="bg-[hsla(0,0%,100%,0.25)] lg:bg-white"></input>;
+  return (
+    <div className="">
+      <input className="bg-[hsla(0,0%,100%,0.25)] lg:bg-white"></input>
+    </div>
+  );
 }
