f5f5408b0bdbc1f0bced542e70a0f6eaf385503e
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-18 15:24:44 -0500

    Gave the input in SearchBar a border on desktop.

--- app/search-bar.tsx
+++ app/search-bar.tsx
@@ -1,7 +1,7 @@
 export default function SearchBar() {
   return (
     <div className="mt-[12px] mx-[24px] mb-[46px] lg:m-0">
-      <input className="bg-[hsla(0,0%,100%,0.25)] lg:bg-white w-full"></input>
+      <input className="bg-[hsla(0,0%,100%,0.25)] lg:bg-white w-full border-none lg:border-1 lg:border-solid lg:border-[#d0d3d4]"></input>
     </div>
   );
 }
