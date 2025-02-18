c899ef9df5f2f7b90c2a6b52d53f82e228e8a964
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-18 15:37:33 -0500

    Set the appropriate font color for the placeholder text on both mobile and desktop.

--- app/search-bar.tsx
+++ app/search-bar.tsx
@@ -3,7 +3,7 @@ export default function SearchBar() {
     <div className="mt-[12px] mx-[24px] mb-[46px] lg:m-0">
       <input
         placeholder="Search"
-        className="bg-[hsla(0,0%,100%,0.25)] lg:bg-white w-full border-none lg:border-1 lg:border-solid lg:border-[#d0d3d4]"
+        className="bg-[hsla(0,0%,100%,0.25)] lg:bg-white w-full border-none lg:border-1 lg:border-solid lg:border-[#d0d3d4] placeholder:text-white lg:placeholder:text-[rgb(13,36,66)]"
       ></input>
     </div>
   );
