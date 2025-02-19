8eaec16d9d506335004b4656344d1ba4f9b4d6d3
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-19 12:02:33 -0500

    Added another div to ToastHero.

--- app/toast-hero.tsx
+++ app/toast-hero.tsx
@@ -2,8 +2,10 @@ export default function ToastHero() {
   return (
     <div>
       <div>
-        <h1>Earn 4.40% APY</h1>
-        <h1>With a high-yield savings account</h1>
+        <div>
+          <h1>Earn 4.40% APY</h1>
+          <h1>With a high-yield savings account</h1>
+        </div>
       </div>
     </div>
   );
