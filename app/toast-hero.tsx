5e6f74c9d6e3d6351fed9092927afd572ead652c
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-19 15:45:22 -0500

    Set the text and background color of the Open an account a tag.

--- app/toast-hero.tsx
+++ app/toast-hero.tsx
@@ -23,7 +23,7 @@ export default function ToastHero() {
           <a>More details</a>
         </div>
         <div className="flex flex-col lg:flex-row items-start">
-          <a className="flex self-center lg:relative lg:mr-[32px] py-[16px] px-[40px]">
+          <a className="flex self-center lg:relative lg:mr-[32px] py-[16px] px-[40px] text-[white] bg-[#1c8195]">
             Open an account
           </a>
           <a className="flex self-center lg:relative py-[16px] px-[40px]">
