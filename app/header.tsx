c083814754a79f8f785516f971bfa874cdbeaeaa
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-18 20:36:05 -0500

    Fixed the bug on mobile where the header was way too big when the SecondaryHeader was closed.

--- app/header.tsx
+++ app/header.tsx
@@ -3,12 +3,18 @@
 import { useState } from "react";
 import PrimaryHeader from "./primary-header";
 import SecondaryHeader from "./secondary-header";
+import classNames from "classnames";
 
 export default function Header() {
   const [isSecondaryOpen, setSecondaryOpen] = useState(false);
   return (
     <div className="h-[128px]">
-      <header className="bg-toast-black lg:bg-white w-full left-[50%] transform-[translate(-50%)] fixed lg:relative z-4 top-0 bottom-0 flex flex-col max-w-[1440px]">
+      <header
+        className={classNames(
+          "bg-toast-black lg:bg-white w-full left-[50%] transform-[translate(-50%)] fixed lg:relative z-4 top-0 flex flex-col max-w-[1440px]",
+          { "bottom-0": isSecondaryOpen }
+        )}
+      >
         <PrimaryHeader
           setSecondaryOpen={() => setSecondaryOpen(true)}
           setSecondaryClosed={() => setSecondaryOpen(false)}
