32db86d29e34281f2901d48afc16f66f5564e1d5
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-19 11:26:12 -0500

    Replace welcome text with a ToastHero component, which for now is just a div.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Header from "./header";
+import ToastHero from "./toast-hero";
 
 export default function Page() {
   return (
     <div className="max-w-[1440px] mx-auto">
       <Header />
       <div className="relative z-1">
-        <h1 className="font-bold underline text-3xl">
-          Welcome to Toast Financial! Automatic deployment is working!
-        </h1>
+        <ToastHero />
       </div>
     </div>
   );
