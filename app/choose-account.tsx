0b45078ecdc946f38e9aa76e8342da2750391340
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-20 16:47:31 -0500

    Changed the divs border when the dropdown is open.

--- app/choose-account.tsx
+++ app/choose-account.tsx
@@ -10,7 +10,10 @@ export default function ChooseAccount() {
     <div className="w-full relative mt-[15px]">
       <div className="mt-[16px] h-[48px]">
         <div
-          className="cursor-pointer p-0 min-w-[303px] flex items-center justify-between bg-white border-[.5px] border-solid border-[#d5d5d5] shadow-[0px_0px_30px_2px_rgba(0,0,0,0.2)] rounded-3xl h-full relative"
+          className={classNames(
+            "cursor-pointer p-0 min-w-[303px] flex items-center justify-between bg-white border-[.5px] border-solid border-[#d5d5d5] shadow-[0px_0px_30px_2px_rgba(0,0,0,0.2)] rounded-3xl h-full relative",
+            { "rounded-none rounded-t-3xl ": isOpen }
+          )}
           onClick={() => setIsOpen(!isOpen)}
         >
           <h2 className="ml-[16px] text-[18px] leading-[24px]">
