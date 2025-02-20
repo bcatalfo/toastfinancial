bbdb084a42c24f451d11f36c415c2fab8a54bbdf
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-20 15:02:38 -0500

    Added a whole bunch of JSX for the Choose your account part of the hero by the Sign in.

--- app/toast-hero.tsx
+++ app/toast-hero.tsx
@@ -50,6 +50,13 @@ export default function ToastHero() {
         <h2 className="text-[28px] lg:text-[36px] font-bold text-toast-black">
           Sign in
         </h2>
+        <div className="w-full relative mt-[15px]">
+          <div className="mt-[16px] h-[48px]">
+            <div className="cursor-pointer p-0 min-w-[303px] flex items-center justify-between bg-white border-[.5px] border-solid border-[#d5d5d5] shadow-[0px_0px_30px_2px_rgba(0,0,0,0.2)] rounded-3xl h-full relative">
+              <h2>Choose your account</h2>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
