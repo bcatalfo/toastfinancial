9a6363c5c10e678edff0729e77703d16c4f0f39e
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-20 15:05:43 -0500

    Added some CSS to the Choose your account h2.

--- app/toast-hero.tsx
+++ app/toast-hero.tsx
@@ -53,7 +53,9 @@ export default function ToastHero() {
         <div className="w-full relative mt-[15px]">
           <div className="mt-[16px] h-[48px]">
             <div className="cursor-pointer p-0 min-w-[303px] flex items-center justify-between bg-white border-[.5px] border-solid border-[#d5d5d5] shadow-[0px_0px_30px_2px_rgba(0,0,0,0.2)] rounded-3xl h-full relative">
-              <h2>Choose your account</h2>
+              <h2 className="ml-[16px] text-[18px] leading-[24px]">
+                Choose your account
+              </h2>
             </div>
           </div>
         </div>
